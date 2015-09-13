d0d4c23de965b8a4ebef06fd94e6e00b69823c97
qife <839259646@qq.com>
2015-09-14 00:32:05 +0800

    修改外键关联信息

--- Models/LoginInfo.cs
+++ Models/LoginInfo.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -12,7 +13,7 @@ namespace DbBasicApp.Models
         public string Name { get; set; }
 
         [Required, Column("password")]
-        [RegularExpression(@"^\w{6,25}$", ErrorMessage="密码只能由字母、数字及下划线组成，长度在6到25位之间。")]
+        [RegularExpression(@"^\w{6,25}$", ErrorMessage = "密码只能由字母、数字及下划线组成，长度在6到25位之间。")]
         public string Password { get; set; }
 
         [Required, Column("user_id")]
@@ -24,13 +25,24 @@ namespace DbBasicApp.Models
 
     // 普通用户登录信息表
     [Table("user_login_info")]
-    public class UserLoginInfo : LoginInfo { }
+    public class UserLoginInfo : LoginInfo
+    {
+        public virtual PaymentRecord PaymentRecord { get; set; }
+
+        public virtual List<RatingRecord> RatingRecord { get; set; }
+    }
 
     // 收款员登录信息表
     [Table("cashier_login_info")]
-    public class CashierLoginInfo : LoginInfo { }
+    public class CashierLoginInfo : LoginInfo
+    {
+        public virtual List<PaymentRecord> PaymentRecords { get; set; }
+    }
 
     // 客服登录信息表
     [Table("supporter_login_info")]
-    public class SupporterLoginInfo : LoginInfo { }
+    public class SupporterLoginInfo : LoginInfo
+    {
+        public virtual List<RatingRecord> RatingRecords { get; set; }
+    }
 }
