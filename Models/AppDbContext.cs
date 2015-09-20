91323762a4a0da645f11ecb02227e6314f4e4024
qife <839259646@qq.com>
2015-09-20 17:15:31 +0800

    通过Fluent API配置外键和唯一约束。

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -41,11 +41,51 @@ namespace DbBasicApp.Models
         /// 评价信息表
         /// </summary>
         public DbSet<RatingRecord> RatingRecords { get; set; }
-        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            base.OnConfiguring(optionsBuilder);
             var appEvn = CallContextServiceLocator.Locator.ServiceProvider.GetRequiredService<IApplicationEnvironment>();
             optionsBuilder.UseSqlite($"Data Source = { appEvn.ApplicationBasePath }/app.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserInfo>().Reference(u => u.TelPackage)
+                .InverseCollection(t => t.UserInfos)
+                .ForeignKey(u => u.PackageID)
+                .Required(false);
+            modelBuilder.Entity<UserLoginInfo>().Reference(u => u.UserInfo)
+                .InverseReference()
+                .ForeignKey((UserLoginInfo u) => u.UserId)
+                .Required(true);
+            modelBuilder.Entity<CashierLoginInfo>().Reference(u => u.UserInfo)
+                .InverseReference()
+                .ForeignKey((CashierLoginInfo u) => u.UserId)
+                .Required(true);
+            modelBuilder.Entity<SupporterLoginInfo>().Reference(u => u.UserInfo)
+                .InverseReference()
+                .ForeignKey((SupporterLoginInfo u) => u.UserId)
+                .Required(true);
+            modelBuilder.Entity<PaymentRecord>().Reference(p => p.UserLoginInfo)
+                .InverseReference()
+                .ForeignKey((PaymentRecord p) => p.UserName)
+                .Required(true);
+            modelBuilder.Entity<PaymentRecord>().Reference(p => p.CashierInfo)
+                .InverseCollection(c => c.PaymentRecords)
+                .ForeignKey(p => p.CashierName)
+                .Required(false);
+            modelBuilder.Entity<RatingRecord>().Reference(r => r.UserLoginInfo)
+                .InverseCollection(u => u.RatingRecords)
+                .ForeignKey(r => r.UserName)
+                .Required(true);
+            modelBuilder.Entity<RatingRecord>().Reference(r => r.SupporterInfo)
+                .InverseCollection(s => s.RatingRecords)
+                .ForeignKey(r => r.SupporterName)
+                .Required(true);
+
+            modelBuilder.Entity<UserInfo>().AlternateKey(u => u.CardID);
         }
     }
 }
