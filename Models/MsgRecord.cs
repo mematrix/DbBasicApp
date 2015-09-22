f84f08e4559b5c3cdf651967090b0764b189ad87
qife <839259646@qq.com>
2015-09-23 01:26:38 +0800

    添加服务消息表

---
+++ Models/MsgRecord.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DbBasicApp.Models
+{
+    // 客服服务消息表
+    [Table("msg_record")]
+    public class MsgRecord
+    {
+        //
+    }
+}
