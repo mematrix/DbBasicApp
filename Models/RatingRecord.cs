60c09d0fe921bb9b571e40d03b14767ab2beb7f0
qife <839259646@qq.com>
2015-09-23 15:42:50 +0800

    添加时间字段。添加显示注解。更新导航属性类型。

--- Models/RatingRecord.cs
+++ Models/RatingRecord.cs
@@ -11,23 +11,29 @@ namespace DbBasicApp.Models
 
         [Required(ErrorMessage = "请选择一个分数"), Column("rating")]
         [Range(1, 5, ErrorMessage = "数值大小超出索引范围")]
+        [Display(Name = "评分")]
         public int Rating { get; set; }
 
-        [Column("rating_msg")]
+        [Column("rating_msg"), DataType(DataType.MultilineText)]
+        [Display(Name = "评价内容")]
         public string RatingMsg { get; set; }
+
+        [Required, Column("time")]
+        public System.DateTime Time { get; set; }
 
         [Required, Column("user_name")]
         public string UserName { get; set; }
 
         // 外键使用Fluent API配置
         // [Required, ForeignKey("UserName")]
-        public virtual UserLoginInfo UserLoginInfo { get; set; }
+        public virtual LoginInfo UserLoginInfo { get; set; }
 
         [Required, Column("supporter_name")]
+        [Display(Name = "客服人员")]
         public string SupporterName { get; set; }
 
         // 外键使用Fluent API配置
         // [Required, ForeignKey("SupporterName")]
-        public SupporterLoginInfo SupporterInfo { get; set; }
+        public virtual LoginInfo SupporterInfo { get; set; }
     }
 }
