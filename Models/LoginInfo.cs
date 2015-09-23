d6e7a8fae18d915b1035645ed298e17eb6a2871e
qife <839259646@qq.com>
2015-09-23 14:49:00 +0800

    修改表结构，添加新字段。添加显示注解。

--- Models/LoginInfo.cs
+++ Models/LoginInfo.cs
@@ -1,29 +1,39 @@
-using System.Collections.Generic;
+// using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DbBasicApp.Models
 {
-    // 登录信息表基类
+    // 登录信息表
+    [Table("login_info")]
     public class LoginInfo
     {
         [Key, Column("name")]
         [RegularExpression(@"^[\w\u4e00-\u9fa5]{3,30}$",
             ErrorMessage = "登录名只能由汉字、数字、字母及下划线组成，并且长度在3到30之间。")]
+        [Display(Name = "登录名")]
         public string Name { get; set; }
 
         [Required, Column("password")]
         [RegularExpression(@"^\w{6,25}$", ErrorMessage = "密码只能由字母、数字及下划线组成，长度在6到25位之间。")]
+        [DataType(DataType.Password)]
+        [Display(Name = "登录密码")]
         public string Password { get; set; }
+
+        // 用户权限级别：0普通用户，1客服人员，2收款员
+        [Required, Column("level")]
+        [Display(Name = "用户身份")]
+        public int Level { get; set; }
 
         [Required, Column("user_id")]
         public int UserId { get; set; }
-        
+
         // 外键使用Fluent API配置
         // [Required, ForeignKey("UserId")]
         public virtual UserInfo UserInfo { get; set; }
     }
 
+    /*
     // 普通用户登录信息表
     [Table("user_login_info")]
     public class UserLoginInfo : LoginInfo
@@ -46,4 +56,5 @@ namespace DbBasicApp.Models
     {
         public virtual List<RatingRecord> RatingRecords { get; set; }
     }
+    */
 }
