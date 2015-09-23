ed10b183e76f28c7116392eb1f46d934dc9a9fa6
qife <839259646@qq.com>
2015-09-23 14:49:42 +0800

    添加注册日期字段。添加显示注解。

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -14,27 +14,39 @@ namespace DbBasicApp.Models
         [Required(ErrorMessage = "姓名为必填项"), Column("name")]
         [RegularExpression(@"^([\u4e00-\u9fa5]{2,18})|((?!\s)[A-Za-z ]{0,30}[A-Za-z])$",
             ErrorMessage = "请输入正确的名称！")]
+        [Display(Name = "姓名")]
         public string Name { get; set; }
 
-        [Column("sex")]
+        [Column("sex"), Display(Name = "性别")]
         public bool? Sex { get; set; }
 
         [Column("birthday"), DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "出生日期")]
         public System.DateTime? Birthday { get; set; }
 
         [Column("card_id")]
         [Remote("IsCardIDAvailable", "Validation")]
+        [Display(Name = "身份证号码")]
         public string CardID { get; set; }
 
         [Required, Column("last_usage")]
+        [Display(Name = "最近一次统计用量")]
         public int LastUsage { get; set; }
 
         [Required, Column("current_usage")]
+        [Display(Name = "当前用量")]
         public int CurrentUsage { get; set; }
 
         [Required, Column("balance")]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = true)]
+        [Display(Name = "账户余额")]
         public int Balance { get; set; }
+
+        [Required, Column("reg_time")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm}")]
+        [Display(Name = "注册时间")]
+        public System.DateTime RegisterTime { get; set; }
 
         [Column("pkg_id")]
         public int? PackageID { get; set; }
