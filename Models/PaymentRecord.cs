d54b75e49d7e84b9d8d4d372d9fe8cdd6e71cbaa
qife <839259646@qq.com>
2015-09-23 15:41:41 +0800

    更新导航属性类型。

--- Models/PaymentRecord.cs
+++ Models/PaymentRecord.cs
@@ -9,28 +9,28 @@ namespace DbBasicApp.Models
     {
         [Key, Column("id")]
         public int ID { get; set; }
-        
+
         [Required, Column("pay_out")]
-        public int PayOut{get;set;}
-        
+        public int PayOut { get; set; }
+
         [Required, Column("time")]
-        public System.DateTime Time{get;set;}
-        
+        public System.DateTime Time { get; set; }
+
         [Column("msg")]
-        public string Msg{get;set;}
-        
+        public string Msg { get; set; }
+
         [Required, Column("user_name")]
-        public string UserName{get;set;}
-        
+        public string UserName { get; set; }
+
         // 外键使用Fluent API配置
         // [Required, ForeignKey("UserName")]
-        public virtual UserLoginInfo UserLoginInfo{get;set;}
-        
+        public virtual LoginInfo UserLoginInfo { get; set; }
+
         [Column("cashier_name")]
-        public string CashierName{get;set;}
-        
+        public string CashierName { get; set; }
+
         // 外键使用Fluent API配置
         // [ForeignKey("CashierName")]
-        public virtual CashierLoginInfo CashierInfo{get;set;}
+        public virtual LoginInfo CashierInfo { get; set; }
     }
 }
