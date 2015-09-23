5c5a2d9b7dac00feeaddf1bbc328920351e0e920
qife <839259646@qq.com>
2015-09-23 15:44:20 +0800

    完善表设计。

--- Models/MsgRecord.cs
+++ Models/MsgRecord.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DbBasicApp.Models
@@ -6,6 +7,25 @@ namespace DbBasicApp.Models
     [Table("msg_record")]
     public class MsgRecord
     {
-        //
+        [Key, Column("id")]
+        public int ID { get; set; }
+
+        [Required, Column("msg")]
+        public string Msg { get; set; }
+
+        [Required, Column("time")]
+        public System.DateTime Time { get; set; }
+
+        [Required, Column("sender_name")]
+        public string SenderName { get; set; }
+
+        // 使用Fluent API配置外键关系
+        public virtual LoginInfo SenderLoginInfo { get; set; }
+
+        [Required, Column("recv_name")]
+        public string ReceiverName { get; set; }
+
+        // 使用Fluent API配置外键关系
+        public virtual LoginInfo RecvLoginInfo { get; set; }
     }
 }
