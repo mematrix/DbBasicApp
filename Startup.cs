cde4aaa3743286422539139238f51824d476f304
qife <839259646@qq.com>
2015-10-01 16:23:35 +0800

    添加和配置服务：EF、自定义服务等。

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DbBasicApp.Models;
+using DbBasicApp.Services;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Dnx.Runtime;
 using Microsoft.Framework.Configuration;
@@ -27,16 +30,21 @@ namespace DbBasicApp
         // This method gets called by the runtime.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFramework()
+                .AddSqlite()
+                .AddDbContext<AppDbContext>();
+            
             // 添加Session
             services.AddCaching();
             services.AddSession();
-            
+
             // Add MVC services to the services container.
             services.AddMvc();
 
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
+            services.AddTransient<AccountService<LoginInfo>>();
         }
 
         // Configure is called after ConfigureServices is called.
@@ -58,11 +66,12 @@ namespace DbBasicApp
                 // Add Error handling middleware which catches all application specific errors and
                 // send the request to the following path or controller action.
                 app.UseErrorHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Home/Status/{0}");
             }
 
             // Add static files to the request pipeline.
             app.UseStaticFiles();
-            
+
             // 启用Session
             app.UseSession();
 
