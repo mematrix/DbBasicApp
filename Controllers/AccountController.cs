4b2c04e37aaa426df5efe16ed24a138f622b1565
qife <839259646@qq.com>
2015-10-01 16:21:59 +0800

    完善登录接口。

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,30 +1,52 @@
-using DbBasicApp.Filters;
+using System.Threading.Tasks;
+using DbBasicApp.Models;
+using DbBasicApp.Services;
 using DbBasicApp.ViewModels;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 
 namespace DbBasicApp.Controllers
 {
     public class AccountController : Controller
     {
+        private AccountService<LoginInfo> _service;
+        private AppDbContext _context;
+        private static bool _dbChecked;
+        
+        public AccountController(AccountService<LoginInfo> service, AppDbContext context)
+        {
+            _service = service;
+            _context = context;
+        }
+        
         public IActionResult Index()
         {
             return RedirectToAction("Login");
         }
         
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
         
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
         {
+            EnsureDatabaseCreated(_context);
+            ViewData["ReturnUrl"] = returnUrl;
             if(ModelState.IsValid)
             {
-                
+                var result = await _service.SignInAsync(model.Name, model.Password, model.RememberMe);
+                if(result.IsSucceeded)
+                {
+                    return RedirectToLocal(returnUrl);
+                }
+                ModelState.AddModelError(string.Empty, "登录失败，请检查账号密码是否正确。");
             }
-            return View();
+            model.Password = null;
+            return View(model);
         }
         
         public IActionResult Register()
@@ -32,10 +54,27 @@ namespace DbBasicApp.Controllers
             return View();
         }
         
-        [CustomAuth]
         public IActionResult Search()
         {
             return View();
+        }
+        
+        private static void EnsureDatabaseCreated(AppDbContext context)
+        {
+            if(!_dbChecked)
+            {
+                _dbChecked = true;
+                context.Database.Migrate();
+            }
+        }
+        
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if(Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
