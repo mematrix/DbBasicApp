38f578fcb1a09e6e563fc2738cce86e420da1287
qife <839259646@qq.com>
2015-10-08 11:41:24 +0800

    更新依赖注入类型注册。

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace DbBasicApp
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
-            services.AddTransient<AccountService<LoginInfo>>();
+            services.AddTransient<AccountService>();
         }
 
         // Configure is called after ConfigureServices is called.
