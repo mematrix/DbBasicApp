2ae28bde2155f9e0407d54a24a1215e3cd2e0af4
qife <839259646@qq.com>
2015-10-08 11:38:37 +0800

    更新约束要求。

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Mvc;
 
@@ -6,12 +5,14 @@ namespace DbBasicApp.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage="登录名不可为空！")]
         [RegularExpression(@"^[\w\u4e00-\u9fa5]{3,30}$",
             ErrorMessage = "登录名只能由汉字、数字、字母及下划线组成，并且长度在3到30之间。")]
         [Remote("IsUserNameExisted", "Validation", HttpMethod = "Post", ErrorMessage = "用户名已存在！")]
         [Display(Name = "登录名")]
         public string UserName { get; set; }
 
+        [Required(ErrorMessage="登录密码不可为空！")]
         [RegularExpression(@"^\w{6,25}$", ErrorMessage = "密码只能由字母、数字及下划线组成，长度在6到25位之间。")]
         [DataType(DataType.Password)]
         [Display(Name = "登录密码")]
@@ -22,6 +23,7 @@ namespace DbBasicApp.ViewModels
         [Display(Name = "确认密码")]
         public string ConfirmPassword { get; set; }
 
+        [Required(ErrorMessage="姓名不可为空！")]
         [RegularExpression(@"^([\u4e00-\u9fa5]{2,18})|((?!\s)[A-Za-z ]{0,30}[A-Za-z])$",
             ErrorMessage = "请输入正确的名称！")]
         [Display(Name = "姓名")]
@@ -38,7 +40,8 @@ namespace DbBasicApp.ViewModels
         [Display(Name = "出生日期")]
         public System.DateTime? Birthday { get; set; }
 
-        [RegularExpression(@"^(\s*)|([1-9]\d{16}[\dxX])$", ErrorMessage = "请输入正确格式的身份证号码！")]
+        [Required(ErrorMessage = "身份证号码必须填写！")]
+        [RegularExpression(@"^[1-9]\d{16}[\dX]$", ErrorMessage = "请输入正确格式的身份证号码！")]
         [Remote("IsCardIDExisted", "Validation", HttpMethod = "Post", ErrorMessage = "您输入的身份证号码已存在！")]
         [Display(Name = "身份证号码")]
         public string CardID { get; set; }
