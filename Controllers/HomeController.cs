017f7083b2608998be958dd0158d371227f0af20
qife <839259646@qq.com>
2015-10-17 00:00:07 +0800

    移除部分导航逻辑。

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,9 @@
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-using DbBasicApp.Filters;
+using Microsoft.Data.Entity;
 using DbBasicApp.Models;
 using DbBasicApp.Services;
-using Microsoft.Data.Entity;
+using DbBasicApp.Util;
 
 namespace DbBasicApp.Controllers
 {
@@ -16,50 +15,10 @@ namespace DbBasicApp.Controllers
         [FromServices]
         public AppDbContext DbContext { get; set; }
 
-        [CustomAuth]
         public async Task<IActionResult> Index()
         {
             var user = await Service.GetCurrentUserAsync();
             return View(user);
-        }
-
-        [CustomAuth]
-        public async Task<IActionResult> GetChildItem(string item)
-        {
-            var viewName = item;
-            object model = null;
-            try
-            {
-                var user = await Service.GetCurrentUserAsync();
-                switch (item.ToLower())
-                {
-                    case "loginview": model = user; break;
-                    case "userinfoview": model = user.UserInfo; break;
-                    case "consumview": model = DbContext.PaymentRecords.Where(p => p.UserName == user.UserName); break;
-                    case "payview":
-                        model = DbContext.PaymentRecords.Where(p => p.UserName == user.UserName && p.PayOut > 0);
-                        break;
-                    case "deductview":
-                        model = DbContext.PaymentRecords.Where(p => p.UserName == user.UserName && p.PayOut < 0);
-                        break;
-                    case "commentview":
-                        model = DbContext.RatingRecords.Where(r => r.UserName == user.UserName)
-                            .OrderByDescending(r => r.Time)
-                            .GroupBy(r => r.SupporterName)
-                            .Select(g => g.First());
-                        break;
-                    default: viewName = "Default"; break;
-                }
-                if (model == null)
-                {
-                    viewName = "Default";
-                }
-                return PartialView("~/Views/Partial/" + viewName + ".cshtml", model);
-            }
-            catch
-            {
-                return PartialView("~/Views/Partial/Default.cshtml");
-            }
         }
 
         public IActionResult About()
@@ -71,6 +30,7 @@ namespace DbBasicApp.Controllers
 
         public async Task<IActionResult> Super()
         {
+            DbHelper.EnsureDatabaseCreated(DbContext);
             var model = await DbContext.LoginInfos.Include(l => l.UserInfo).ToListAsync();
             return View(model);
         }
@@ -79,10 +39,10 @@ namespace DbBasicApp.Controllers
         public async Task<JsonResult> SetLevel(string userName, int level)
         {
             if (level > 2 || level < 0) return Json(new { status = false, msg = "错误的身份设置！" });
-            
+
             var user = await DbContext.LoginInfos.FirstOrDefaultAsync(l => l.UserName == userName);
             if (user == null) return Json(new { status = false, msg = "用户不存在！" });
-            
+
             var update = user.Level != level;
             user.Level = level;
             DbContext.LoginInfos.Update(user);
