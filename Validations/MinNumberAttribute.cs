eccec789eb41c04a857d3e867d129a27923b2149
qife <839259646@qq.com>
2015-10-18 05:21:34 +0800

    完善数据验证注解，添加客户端验证，修复bug。

--- Validations/MinNumberAttribute.cs
+++ Validations/MinNumberAttribute.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Mvc.ModelBinding.Validation;
 
 namespace DbBasicApp.Validations
 {
@@ -7,14 +9,33 @@ namespace DbBasicApp.Validations
     // 验证属性／字段／参数等的值不小于指定的值（仅限于数值）
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
         AllowMultiple = false)]
-    public class MinNumberAttribute : ValidationAttribute
+    public class MinNumberAttribute : ValidationAttribute, IClientModelValidator 
     {
         public double MinValue { get; set; } = 0;
 
-        public override bool IsValid(object value)
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ClientModelValidationContext context)
         {
-            double v = 0.0;
-            return double.TryParse(value as string, out v) && v >= MinValue;
+            var rule = new ModelClientValidationRule("minnumber", 
+                this.FormatErrorMessage(context.ModelMetadata.DisplayName));
+            rule.ValidationParameters.Add("minvalue", MinValue);
+            yield return rule;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            try
+            {
+                var num = Convert.ToDouble(value);
+                if (num >= MinValue)
+                {
+                    return ValidationResult.Success;
+                }
+                return new ValidationResult(this.FormatErrorMessage(context.DisplayName));
+            }
+            catch
+            {
+                return new ValidationResult(this.FormatErrorMessage(context.DisplayName));
+            }
         }
     }
 }
